f9e25fa6e7e8ef4db238f150ede78871d3e9d589
dk-4999 <dk4999@nyu.edu>
2025-05-10 17:51:06 -0400

    changes to tf files

--- terraform/kvm/newmain.tf
+++ terraform/kvm/newmain.tf
@@ -1,32 +1,21 @@
-# Create private network
-resource "openstack_networking_network_v2" "private_network" {
-  name           = var.network_name
-  admin_state_up = true
-}
 
-# Create subnet
-resource "openstack_networking_subnet_v2" "private_subnet" {
-  name            = "private_subnet_project15"
-  network_id      = openstack_networking_network_v2.private_network.id
-  cidr            = "192.168.17.0/24"
-  ip_version      = 4
-  gateway_ip      = "192.168.17.1"
-  dns_nameservers = ["8.8.8.8", "1.1.1.1"]
+# Pull network info
+data "openstack_networking_network_v2" "network" {
+  name = var.network_name
 }
 
 # Keypair
 resource "openstack_compute_keypair_v2" "keypair" {
   name       = var.keypair_name
-  public_key = file(var.public_key_path)
 }
 
-# Dedicated port for controller
+# Create dedicated port for controller
 resource "openstack_networking_port_v2" "controller_port" {
-  name       = "controller-port-project15"
-  network_id = openstack_networking_network_v2.private_network.id
+  name       = "controller-port-group15"
+  network_id = data.openstack_networking_network_v2.network.id
 }
 
-# Floating IP for controller
+# Floating IP associated with port
 resource "openstack_networking_floatingip_v2" "controller_fip" {
   pool    = "public"
   port_id = openstack_networking_port_v2.controller_port.id
@@ -34,7 +23,7 @@ resource "openstack_networking_floatingip_v2" "controller_fip" {
 
 # Controller node
 resource "openstack_compute_instance_v2" "controller" {
-  name            = "controller-project15"
+  name            = "controller-group15"
   image_name      = var.image_name
   flavor_name     = var.flavor_name
   key_pair        = openstack_compute_keypair_v2.keypair.name
@@ -45,16 +34,16 @@ resource "openstack_compute_instance_v2" "controller" {
   }
 }
 
-# Worker nodes (on private network)
+# Worker nodes (no floating IP)
 resource "openstack_compute_instance_v2" "worker" {
   count           = 2
-  name            = "worker${count.index}-project15"
+  name            = "worker${count.index}-group15"
   image_name      = var.image_name
   flavor_name     = var.flavor_name
   key_pair        = openstack_compute_keypair_v2.keypair.name
   security_groups = ["default"]
 
   network {
-    uuid = openstack_networking_network_v2.private_network.id
+    name = var.network_name
   }
 }
---
+++ terraform/kvm/newvariable.tf
@@ -0,0 +1,91 @@
+variable "create_kvm_node" {
+  description = "Set to true to provision the KVM node at KVM@TACC"
+  type        = bool
+  default     = true
+}
+
+variable "create_gpu_node" {
+  description = "Set to true to provision the node at CHI@TACC"
+  type        = bool
+  default     = true
+}
+
+variable "ssh_key_pair_name" {
+  description = "Name of the SSH key pair registered in OpenStack (must exist in both regions)"
+  type        = string
+  default     = "mlops-key-15"
+}
+
+variable "kvm_image_name" {
+  description = "Image name for the KVM node (KVM@TACC)"
+  type        = string
+  default     = "CC-Ubuntu22.04"
+}
+
+variable "kvm_flavor_name" {
+  description = "Flavor for the KVM node (KVM@TACC)"
+  type        = string
+  default     = "m1.large" 
+}
+
+variable "gpu_image_name" {
+  description = "Image name for the node at CHI@TACC"
+  type        = string
+  default     = "CC-Ubuntu22.04"
+}
+
+variable "gpu_flavor_name" {
+  description = "Flavor for the node at CHI@TACC (Use m1.large for now, will change to GPU bare metal later)"
+  type        = string
+  default     = "baremetal"
+}
+
+variable "gpu_lease_id" {
+  description = "Lease ID for a baremetal at CHI site"
+  type        = string
+  default     = "5626cb8b-3e92-4fbc-9594-3808d1809e30"
+}
+
+variable "network_name" {
+  description = "Name of the network to attach instances to"
+  type        = string
+  default     = "sharednet1"
+}
+
+variable "external_network_name" {
+  description = "Name of the external network for Floating IPs"
+  type        = string
+  default     = "public"
+}
+
+variable "ssh_private_key_content" {
+  description = "Content of the private SSH key for connecting to instances."
+  type        = string
+  sensitive   = true
+}
+
+
+variable "kvm_volume_size" {
+  description = "Size of the persistent block storage volume in GB for the KVM node"
+  type        = number
+  default     = 50 # Adjust size as needed
+}
+
+# --- KVM Node Service Credentials (MLflow/MinIO) ---
+variable "minio_root_user" {
+  description = "Root username for MinIO"
+  type        = string
+  sensitive   = true
+}
+
+variable "minio_root_password" {
+  description = "Root password for MinIO"
+  type        = string
+  sensitive   = true
+}
+
+# Keypair Variables
+variable "keypair_name" {
+  description = "id_rsa_chameleon"
+  type        = string
+}
